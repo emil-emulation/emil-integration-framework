461f0381d7642dd1b97722afc7899250e69fe8a0
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2016-03-21 16:09:07 +0100

    Added test.

---
+++ src/test/java/de/multimedia/emulation/emil/integration/demo/service/DemoServiceTest.java
@@ -0,0 +1,19 @@
+package de.multimedia.emulation.emil.integration.demo.service;
+
+import de.multimedia.emulation.emil.integration.demo.config.DemoConfig;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.Test;
+
+
+public class DemoServiceTest {
+
+  @Test
+  public void demoServiceShouldGetObjectIdFromConfig() {
+    DemoConfig config = new DemoConfig();
+    config.setObjectId("ID12345");
+    DemoService demoService = new DemoService(config);
+    assertThat(demoService.getDemoObject().getId(), is("IE12345"));
+  }
+
+}
