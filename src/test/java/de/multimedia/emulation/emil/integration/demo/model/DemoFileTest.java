52387db223bef1651e6c2398d0828e148ef24297
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2016-04-06 13:53:41 +0200

    Removed unused code.

--- src/test/java/de/multimedia/emulation/emil/integration/demo/model/DemoFileTest.java
+++ src/test/java/de/multimedia/emulation/emil/integration/demo/model/DemoFileTest.java
@@ -1,7 +1,6 @@
 package de.multimedia.emulation.emil.integration.demo.model;
 
 import org.agileware.test.PropertiesTester;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -9,11 +8,6 @@ import org.junit.Test;
  * @author Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
  */
 public class DemoFileTest {
-
-  public DemoFileTest() {}
-
-  @Before
-  public void setUp() {}
 
   @Test
   public void propertiesShouldWork() throws Exception {
