57b82c9a36527afd6ef04b34e71ab0caf9a42211
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2016-04-06 15:09:55 +0200

    Added tests.

--- src/test/java/de/multimedia/emulation/emil/integration/demo/model/DemoFileTest.java
+++ src/test/java/de/multimedia/emulation/emil/integration/demo/model/DemoFileTest.java
@@ -1,6 +1,11 @@
 package de.multimedia.emulation.emil.integration.demo.model;
 
+import de.multimedia.emulation.emil.integration.model.object.FileType;
 import org.agileware.test.PropertiesTester;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 /**
@@ -13,6 +18,20 @@ public class DemoFileTest {
   public void propertiesShouldWork() throws Exception {
     PropertiesTester propertiesTester = new PropertiesTester();
     propertiesTester.testAll(DemoFile.class);
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void getUrlShouldThrowExceptionForMalformedUrl() {
+    DemoFile demoFile = new DemoFile();
+    demoFile.setUrl("abc");
+    assertThat(demoFile.getUrl(), is(not(anything())));
+  }
+
+  @Test
+  public void getTypeShouldReturnEnum() {
+    DemoFile demoFile = new DemoFile();
+    demoFile.setType("iso");
+    assertThat(demoFile.getType(), is(FileType.ISO));
   }
 
 }
